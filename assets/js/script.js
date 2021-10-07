71d011cbd076cbee29c9969a9427ecaf1b1eecb4
Cornelia Herman <corneliabherman@gmail.com>
2021-10-06 23:40:52 -0400

    updated fucntions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
 function hotelAPI(inputLocation) {
 	inputLocation = 'Atlanta';
+	fetch("https://hotels4.p.rapidapi.com/locations/search?query=" + inputLocation + "&locale=en_US", {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "hotels4.p.rapidapi.com",
+			"x-rapidapi-key": "47dd23d239msh1ddfbdbd3bd2081p1fcc71jsn32fd8fabb7c8"
+		}
+	})
+		.then(response => {
+			console.log(response);
+		})
+		.catch(err => {
+			console.error(err);
+		});
 }
 
 function getRestaurantAPI(city, meters) {
